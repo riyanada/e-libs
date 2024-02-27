bd579d1f10a7af06f3c5fbd9b25ee6f36ecdf298
riyanada <riyanmaulana402@yahoo.co.id>
2024-02-27 17:03:17 +0700

    update index

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>E-Libs JCC Batch 2</title>
+    <title>E-Libs UTS WEB 2</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('theme/images/logo-elibs.png')}}">
     <!-- Custom Stylesheet -->
